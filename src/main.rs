50c4ac85cac86639d948969f8910b5e43412c862
Vojtěch Toman <cry.wolf@centrum.cz>
2024-03-26 20:01:44 +0100

    Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream)?
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let response = b"HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response)
 }
